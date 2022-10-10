f33fe79173a7e76c2469792fd7a9e46db5c3ef2f
nakzyu <59201980+nakzyu@users.noreply.github.com>
2022-10-10 17:15:37 +0900

    feat: merge content component, remove default streamers

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,11 @@ import { Observer } from 'mobx-react';
 import { ChakraProvider, theme } from '@chakra-ui/react';
 import Nav from './Components/Nav';
 import Content from './Components/Content';
-import ContentFull from './Components/ContentFull';
-import configStore from './stores/ConfigStore';
 
 function App() {
-  const [broadcastMember, setBroadcastMember] = useState(1);
-  const [broadcastMemberList, setBroadcastMemberList] = useState([]);
   const [selectedUser, setSelectedUser] = useState([]);
 
   // on Fail => Sentry.captureException(e);
-
-  const renderChat = () =>
-    configStore.displayEachChat ? (
-      <ContentFull data={{ selectedUser, broadcastMemberList }} />
-    ) : (
-      <Content data={{ selectedUser, broadcastMemberList }} />
-    );
 
   return (
     <ChakraProvider theme={theme}>
@@ -26,11 +15,9 @@ function App() {
         data={{
           selectedUser,
           setSelectedUser,
-          broadcastMember,
-          broadcastMemberList,
         }}
       ></Nav>
-      <Observer>{renderChat}</Observer>
+      <Content data={{ selectedUser }} />
     </ChakraProvider>
   );
 }
--- src/Components/Content.tsx
+++ src/Components/Content.tsx
@@ -5,6 +5,7 @@ import {
   GridItem,
   useColorModeValue,
   Button,
+  Flex,
 } from '@chakra-ui/react';
 import './button-style.css';
 import configStore from '../stores/ConfigStore';
@@ -12,37 +13,6 @@ import { Observer } from 'mobx-react';
 
 export default function Content({ data }: { data: any }) {
   const [chatUser, setChatUser] = useState('');
-
-  const changeNameToKor = (name: string) => {
-    switch (name) {
-      case 'gamjagabee':
-        return '가비';
-      case 'gunaguna00':
-        return '구나';
-      case 'beadyo97':
-        return '슬요';
-      case 'vnek1234':
-        return '도란';
-      case 'kiwi4381':
-        return '키위';
-      case 'kbs9981':
-        return '병살';
-      case 'adricham':
-        return '리챔';
-      case 'yudarlinn':
-        return '달린';
-      case 'gofl2237':
-        return '해리';
-      case 'jeeya0402':
-        return '지야';
-      case 'kimc6h12o6':
-        return '도당';
-      case 'nyangoooong':
-        return '권냥';
-      default:
-        return '';
-    }
-  };
 
   useEffect(() => {
     if (data.selectedUser.length > 0) {
@@ -98,128 +68,120 @@ export default function Content({ data }: { data: any }) {
   let currCnt = 0;
 
   return (
-    <Observer>
-      {() => (
+    <Box
+      minH="calc(100vh - 64px)"
+      bg={configStore.darkModeEnabled ? 'gray.800' : 'gray.100'}
+      style={{ display: 'flex', justifyContent: 'space-between' }}
+    >
+      <Flex>
         <Box
-          minH="calc(100vh - 64px)"
-          bg={configStore.darkModeEnabled ? 'gray.800' : 'gray.100'}
-          style={{ display: 'flex', justifyContent: 'space-between' }}
-        >
-          {data.selectedUser.length ? (
-            <>
-              <Box
-                style={
-                  data.hideChat
-                    ? {
-                        width: '100vw',
-                        height: 'calc(100vh - 64px)',
-                      }
-                    : {
-                        width: 'calc(100vw - 310px)',
-                        height: 'calc(100vh - 64px)',
-                      }
+          style={
+            data.hideChat
+              ? {
+                  width: '100vw',
+                  height: 'calc(100vh - 64px)',
                 }
-              >
-                <Grid
-                  templateRows={templateRows}
-                  templateColumns={templateColumns}
-                  style={{ height: '100%' }}
+              : {
+                  width: 'calc(100vw - 310px)',
+                  height: 'calc(100vh - 64px)',
+                }
+          }
+        >
+          <Grid
+            templateRows={templateRows}
+            templateColumns={templateColumns}
+            style={{ height: '100%' }}
+          >
+            {data.selectedUser.map((user: string) => {
+              return (
+                <GridItem
+                  key={user}
+                  w="100%"
+                  h="100%"
+                  colSpan={
+                    data.selectedUser.length === ++currCnt ||
+                    (data.selectedUser.length === 10 && currCnt >= 9)
+                      ? lastOne
+                      : 1
+                  }
                 >
-                  {data.selectedUser.map((user: string) => {
-                    return (
-                      <GridItem
-                        key={user}
-                        w="100%"
-                        h="100%"
-                        colSpan={
-                          data.selectedUser.length === ++currCnt ||
-                          (data.selectedUser.length === 10 && currCnt >= 9)
-                            ? lastOne
-                            : 1
-                        }
-                      >
-                        <iframe
-                          title="embed"
-                          id={'embed_' + user}
-                          src={
-                            'https://player.twitch.tv/?muted=true&channel=' +
-                            user +
-                            '&parent=localhost&parent=multi.leaven.team&parent=dev-multi.leaven.team'
-                          }
-                          className="stream"
-                          style={{
-                            width: '100%',
-                            height: '100%',
-                          }}
-                        ></iframe>
-                      </GridItem>
-                    );
-                  })}
-                </Grid>
-              </Box>
-              {data.selectedUser.length !== 0 && !data.hideChat && (
-                <Box
-                  style={{
-                    height:
-                      'calc(100vh - ' +
-                      (data.selectedUser.length > 1 ? 94 : 64) +
-                      'px)',
-                    width: '340px',
-                  }}
-                >
-                  {data.selectedUser.length > 1 && (
-                    <Box style={{ height: '30px' }}>
-                      {data.selectedUser.map((user: string) => {
-                        return (
-                          <Button
-                            key={user}
-                            h={30}
-                            size="xs"
-                            style={{ padding: '5px' }}
-                            colorScheme={
-                              chatUser === user
-                                ? 'teal'
-                                : configStore.darkModeEnabled
-                                ? 'blackAlpha'
-                                : 'gray'
-                            }
-                            onClick={() => {
-                              setChatUser(user);
-                            }}
-                          >
-                            {changeNameToKor(user)}
-                          </Button>
-                        );
-                      })}
-                    </Box>
-                  )}
-                  <iframe
-                    title="chat"
-                    scrolling="no"
-                    id={'chat-' + chatUser + '-embed'}
-                    src={
-                      'https://twitch.tv/embed/' +
-                      chatUser +
-                      (configStore.darkModeEnabled
-                        ? '/chat?darkpopout&parent=localhost&parent=multi.leaven.team&parent=dev-multi.leaven.team'
-                        : '/chat?parent=localhost&parent=multi.leaven.team&parent=dev-multi.leaven.team')
-                    }
-                    height="100%"
-                    width="100%"
-                  ></iframe>
-                </Box>
-              )}
-            </>
-          ) : (
-            <Box
-              style={{
-                width: 'calc(100vw - 310px)',
-                height: 'calc(100vh - 64px)',
-              }}
-            ></Box>
-          )}
+                  <Flex style={{ height: '100%' }}>
+                    <iframe
+                      title="embed"
+                      id={'embed_' + user}
+                      src={
+                        'https://player.twitch.tv/?muted=true&channel=' +
+                        user +
+                        '&parent=localhost&parent=multi.leaven.team&parent=dev-multi.leaven.team'
+                      }
+                      className="stream"
+                      style={{
+                        width: '100%',
+                        height: '100%',
+                      }}
+                    ></iframe>
+                    <iframe
+                      title="chat"
+                      scrolling="no"
+                      id={'chat-' + user + '-embed'}
+                      src={
+                        'https://twitch.tv/embed/' +
+                        user +
+                        '/chat?parent=localhost&parent=multi.leaven.team&parent=dev-multi.leaven.team'
+                      }
+                      width="340px"
+                      height="100%"
+                      allowFullScreen={true}
+                    ></iframe>
+                  </Flex>
+                </GridItem>
+              );
+            })}
+          </Grid>
         </Box>
-      )}
-    </Observer>
+        <Box>
+          {data.selectedUser.length > 1 && (
+            <Box style={{ height: '30px' }}>
+              {data.selectedUser.map((user: string) => {
+                return (
+                  <Button
+                    key={user}
+                    h={30}
+                    size="xs"
+                    style={{ padding: '5px' }}
+                    colorScheme={
+                      chatUser === user
+                        ? 'teal'
+                        : configStore.darkModeEnabled
+                        ? 'blackAlpha'
+                        : 'gray'
+                    }
+                    onClick={() => {
+                      setChatUser(user);
+                    }}
+                  >
+                    {user}
+                  </Button>
+                );
+              })}
+            </Box>
+          )}
+          <iframe
+            title="chat"
+            scrolling="no"
+            id={'chat-' + chatUser + '-embed'}
+            src={
+              'https://twitch.tv/embed/' +
+              chatUser +
+              (configStore.darkModeEnabled
+                ? '/chat?darkpopout&parent=localhost&parent=multi.leaven.team&parent=dev-multi.leaven.team'
+                : '/chat?parent=localhost&parent=multi.leaven.team&parent=dev-multi.leaven.team')
+            }
+            height="100%"
+            width="100%"
+          ></iframe>
+        </Box>
+      </Flex>
+    </Box>
   );
 }
